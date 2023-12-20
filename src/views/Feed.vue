bfd76e142dc4c06b21867768d1bb4f6e0ee9ebca
szymonkonopek <sk.gornik@gmail.com>
2023-12-20 13:30:49 +0100

    new note wip

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,12 +1,14 @@
 <template>
   <div>Feed</div>
   <NotesList :uid="'9I4pFBfpeKS3gBh7FTdJIJki5g92'"></NotesList>
+  <NewNoteButton></NewNoteButton>
 </template>
 
 <script>
 import NotesList from "@/components/NotesList.vue";
+import NewNoteButton from "@/components/NewNoteButton.vue";
 export default {
   name: "AppFeedView",
-  components: { NotesList },
+  components: { NotesList, NewNoteButton },
 };
 </script>
