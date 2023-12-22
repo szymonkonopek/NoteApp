7990b77c1ce48b3bbbc9a010ad03ed4c5578bcff
AmeliaLis <lis.amelia.maria@gmail.com>
2023-12-22 18:18:36 +0100

    lowered login and register view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,38 @@
 <template>
-  
-<div class="container-fluid justify-content-center text-center px-4 py-2 mx-auto my-4">
-  <div class="card justify-content-center text-center mx-auto" style="width: 30rem; background: rgba(255, 255, 255, 0.19);border-radius: 16px;box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);backdrop-filter: blur(8px);-webkit-backdrop-filter: blur(8px);border: 1px solid rgba(255, 255, 255, 0.46);">
-    <div class="card-body">
-      <div>
-        <h1>Login</h1>
-        <div class="py-2 my-2">
-         
-          <div class="input-group input-group-lg py-2">
-            <span class="input-group-text" id="inputGroup-sizing-lg">Email</span>
-            <input type="text" class="form-control" placeholser="Email" v-model="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+<div style="height: 50%">
+  <div style="height: 5vw"></div>
+  <div class="container-fluid justify-content-center text-center px-4 py-2 mx-auto my-4">
+    <div class="card justify-content-center text-center mx-auto" style="width: 30rem; background: rgba(255, 255, 255, 0.19);border-radius: 16px;box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);backdrop-filter: blur(8px);-webkit-backdrop-filter: blur(8px);border: 1px solid rgba(255, 255, 255, 0.81);">
+      <div class="card-body">
+        <div>
+          <h1>Login</h1>
+          <div class="py-2 my-2">
+          
+            <div class="input-group input-group-lg py-2">
+              <span class="input-group-text" id="inputGroup-sizing-lg">Email</span>
+              <input type="text" class="form-control" placeholser="Email" v-model="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+            </div>
+            <div class="input-group input-group-lg py-2">
+              <span class="input-group-text" id="inputGroup-sizing-lg">Password</span>
+              <input type="password" class="form-control" placeholser="Password" v-model="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+            </div>
+            <div>
+              <button
+                class="btn btn-lg btn-outline-dark my-2"
+                @click="login"
+              >
+                Submit
+              </button>
+            </div>
+            <div>
+              <button
+                class="btn btn-lg btn-outline-dark my-2"
+                @click="signInWithGoogle"
+              >
+                Sign in with Google
+              </button>
+            </div> 
           </div>
-          <div class="input-group input-group-lg py-2">
-            <span class="input-group-text" id="inputGroup-sizing-lg">Password</span>
-            <input type="password" class="form-control" placeholser="Password" v-model="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-          </div>
-          <div>
-            <button
-              class="btn btn-lg btn-outline-dark my-2"
-              @click="login"
-            >
-              Submit
-            </button>
-          </div>
-          <div>
-            <button
-              class="btn btn-lg btn-outline-dark my-2"
-              @click="signInWithGoogle"
-            >
-              Sign in with Google
-            </button>
-          </div> 
         </div>
       </div>
     </div>
