f8e92b259a5ca362acc2dd2f145d690af9341aef
malwinakochman <malwinakochman@gmail.com>
2023-12-23 00:43:55 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <app-top-bar></app-top-bar>
     <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppTopBar from "@/components/TopBar.vue";
+import AppFooter from "@/components/Footer";
 export default {
   name: "App",
   components: {
     AppTopBar,
+    AppFooter
   },
 };
 </script>
