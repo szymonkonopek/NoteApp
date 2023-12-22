1465181c4a4316d8d830a436d42e4def4cf0152e
AmeliaLis <lis.amelia.maria@gmail.com>
2023-12-22 19:54:38 +0100

    delete button on users' note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -7,17 +7,44 @@
     <div v-for="(tag, index) in note.data.tags" :key="index">
       <span class="badge rounded-pill bg-primary">tag : {{ tag }}</span>
     </div>
+    <div>
+      <DeleteButton v-if="isNoteOwner"></DeleteButton>
+    </div>
   </div>
 </template>
 
 <script>
+import { getAuth } from 'firebase/auth';
+import DeleteButton from './DeleteButton.vue';
+
 export default {
-  name: "AppNotesView",
-  props: {
-    note: {
-      type: Object,
-      required: true,
+    name: "AppNotesView",
+    props: {
+        note: {
+          type: Object,
+          required: true,
+        },
     },
-  },
+    data() {
+      return {
+        auth: "",
+        currentUserId: ""
+      }
+    },
+    components: { DeleteButton },
+    mounted() {
+      this.auth = getAuth;
+      this.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.currentUserId = user.uid;
+        }
+      });
+    },
+
+    computed: {
+      isNoteOwner() {
+        return this.note.uid === this.currentUserId
+      }
+    }
 };
 </script>
